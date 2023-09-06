6f30c7dee4338cf60e50309a1fbd0563475c2427
S.SALMAAN  MOHAMED <124113998+salmaanmohamed@users.noreply.github.com>
2023-09-06 19:13:21 +0530

    Add files via upload

---
+++ unit 1 challanges/factorial.py
@@ -0,0 +1,17 @@
+def factorial(n):
+  if n == 0:
+    return 1
+  else:
+    return n * factorial(n - 1)
+
+
+# Input a number
+num = int(input("Enter a number: "))
+
+# Check if the number is negative
+if num < 0:
+  print("Factorial is not defined for negative numbers.")
+elif num == 0:
+  print("The factorial of 0 is 1")
+else:
+  print(f"The factorial of {num} is {factorial(num)}")
